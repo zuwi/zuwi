ad20a7e3dfb6cc97a9821c1434c2254b99611e9d
jannis.wingeyer <jannis.wingeyer@gmail.com>
2019-01-24 20:11:28 +0100

    Initial commit

---
+++ zuwi/zuwi/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace zuwi
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
